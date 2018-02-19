e054ad4bd32cb2f2beb59ee4cdc48e84ef38e51f
eugene1988 <eugene@oneplanetops.com>
2018-02-19 11:31:21 +0200

    minor fix

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import loaderIcon from 'assets/icons/ReactHeart.svg';
 import './style.scss';
-import {Transition} from 'react-transition-group';
 
 const Loader = props => {
   return (
     <div className='loader'>
-      <Transition>
-        <div className='loader__wrapper'>
-          <img src={loaderIcon} alt='' className='loader__img'/>
-        </div>
-      </Transition>
+      <div className='loader__wrapper'>
+        <img src={loaderIcon} alt='' className='loader__img'/>
+      </div>
     </div>
   );
 };
