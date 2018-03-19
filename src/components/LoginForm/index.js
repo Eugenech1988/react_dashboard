029f97afb72ac14094a7dbe0071a1c1f4ecca014
eugenech1988 <eugenech1988@gmail.com>
2018-03-19 09:40:00 +0200

    google btn and library added

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 import FacebookLogin from 'components/FacebookLogin';
+import GoogleLogin from 'components/GoogleLogin';
 import Button from 'components/Button';
 import FBIcon from 'assets/icons/facebook.svg';
+import GIcon from 'assets/icons/google.svg';
 
 const mapStateToProps = (state) => ({
 });
@@ -14,29 +16,30 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 @connect(mapStateToProps, mapDispatchToProps)
-  
-  
+
+
 class LoginForm extends Component {
   state = {
-    username: null
+    FBUsername: null,
+    GUsername: null,
   };
-  
+
   onFacebookLogin = (loginStatus, resultObject) => {
     console.log(resultObject);
     if (loginStatus === true) {
       this.setState({
-        username: resultObject.user.name
+        FBUsername: resultObject.user.name
       });
     } else {
       alert('Facebook login error');
     }
   };
-  
+
   render() {
-    const {username} = this.state;
+    const {FBUsername, GUsername} = this.state;
     return (
       <div className='login-form'>
-        {!username &&
+        {!FBUsername && !GUsername &&
         < FacebookLogin onLogin={this.onFacebookLogin}>
           <Button
             login
@@ -46,11 +49,28 @@ class LoginForm extends Component {
           />
         </FacebookLogin>
         }
-        {username &&
-          <p className='login-form__user-notify'>
-            Welcome back
-            <b>{username}</b>
-          </p>
+        {!FBUsername && !GUsername &&
+        <div className='login-form__separator-wrap'>
+          <span className='login-form__separator'>
+            OR
+          </span>
+        </div>
+        }
+        {!FBUsername && !GUsername &&
+        <GoogleLogin>
+          <Button
+            login
+            btnImgSrc={GIcon}
+            aditionalCls='google-btn'
+            btnText='connect to google'
+          />
+        </GoogleLogin>
+        }
+        {FBUsername &&
+        <p className='login-form__user-notify'>
+          Welcome back
+          <b>{FBUsername}</b>
+        </p>
         }
       </div>
     );
